717f51818d883213f663001c78d8886084d294f3
Chris Angelico <rosuav@gmail.com>
2021-12-30 07:33:45 +1100

    Fail-and-bail to remove an indentation level

--- ArmstrongNav.cs
+++ ArmstrongNav.cs
@@ -10,25 +10,23 @@ namespace Rosuav {
 		private float last_update = 0.0f;
 		void FixedUpdate()
 		{
-			if ((Time.time - last_update) > 1.0f)
-			{
-				last_update = Time.time;
-				Vessel self = FlightGlobals.ActiveVessel;
-				if (!self) return;
-				FinePrint.Waypoint waypoint = self.navigationWaypoint;
-				CelestialBody surface = self.mainBody;
-				if (waypoint == null || surface == null) return;
-				double srfvel = self.GetSrfVelocity().magnitude;
-				//Calculate great circle distance from lat/long and radius
-				double lat1 = self.latitude * Math.PI / 180, lat2 = waypoint.latitude * Math.PI / 180;
-				double longs = (self.longitude - waypoint.longitude) * Math.PI / 180;
-				double angle = Math.Acos(Math.Sin(lat1) * Math.Sin(lat2) +
-					Math.Cos(lat1) * Math.Cos(lat2) * Math.Cos(longs));
-				double circle_dist = angle * surface.Radius;
-				//TODO: Add the estimated time to the current mission time and report estimated arrival time
-				print(String.Format("[ArmstrongNav] {0:0.00} deg or {1:0} m at {2:0.00} m/s = {3:0.00} sec",
-					angle * 180 / Math.PI, circle_dist, srfvel, circle_dist / srfvel));
-			}
+			if (Time.time - last_update < 1.0f) return;
+			last_update = Time.time;
+			Vessel self = FlightGlobals.ActiveVessel;
+			if (!self) return;
+			FinePrint.Waypoint waypoint = self.navigationWaypoint;
+			CelestialBody surface = self.mainBody;
+			if (waypoint == null || surface == null) return;
+			double srfvel = self.GetSrfVelocity().magnitude;
+			//Calculate great circle distance from lat/long and radius
+			double lat1 = self.latitude * Math.PI / 180, lat2 = waypoint.latitude * Math.PI / 180;
+			double longs = (self.longitude - waypoint.longitude) * Math.PI / 180;
+			double angle = Math.Acos(Math.Sin(lat1) * Math.Sin(lat2) +
+				Math.Cos(lat1) * Math.Cos(lat2) * Math.Cos(longs));
+			double circle_dist = angle * surface.Radius;
+			//TODO: Add the estimated time to the current mission time and report estimated arrival time
+			print(String.Format("[ArmstrongNav] {0:0.00} deg or {1:0} m at {2:0.00} m/s = {3:0.00} sec",
+				angle * 180 / Math.PI, circle_dist, srfvel, circle_dist / srfvel));
 		}
 	}
 }
