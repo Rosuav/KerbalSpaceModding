7ba5fffedca800b596839a8a95cb7d6f54d19cef
Chris Angelico <rosuav@gmail.com>
2021-12-30 07:37:55 +1100

    Add the destination name

--- ArmstrongNav.cs
+++ ArmstrongNav.cs
@@ -25,8 +25,8 @@ namespace Rosuav {
 				Math.Cos(lat1) * Math.Cos(lat2) * Math.Cos(longs));
 			double circle_dist = angle * surface.Radius;
 			//TODO: Add the estimated time to the current mission time and report estimated arrival time
-			print(String.Format("[ArmstrongNav] {0:0.00} deg or {1:0} m at {2:0.00} m/s = {3:0.00} sec",
-				angle * 180 / Math.PI, circle_dist, srfvel, circle_dist / srfvel));
+			print(String.Format("[ArmstrongNav] {0:0.00} deg or {1:0} m at {2:0.00} m/s = {3:0.00} sec - {4}",
+				angle * 180 / Math.PI, circle_dist, srfvel, circle_dist / srfvel, waypoint.name));
 		}
 	}
 }
