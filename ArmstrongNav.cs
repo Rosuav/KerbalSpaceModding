cf59bbb063b404494ee8f1ba21616268a466571d
Chris Angelico <rosuav@gmail.com>
2021-12-30 07:32:56 +1100

    Remove vector calculations and just use great circle distance

--- ArmstrongNav.cs
+++ ArmstrongNav.cs
@@ -19,22 +19,12 @@ namespace Rosuav {
 				CelestialBody surface = self.mainBody;
 				if (waypoint == null || surface == null) return;
 				double srfvel = self.GetSrfVelocity().magnitude;
-				//Technique 1: Get surface-relative positions and do vector arithmetic
-				Vector3d here = surface.GetRelSurfacePosition(self.latitude, self.longitude, 0.0);
-				Vector3d there = surface.GetRelSurfacePosition(waypoint.latitude, waypoint.longitude, 0.0);
-				double dist = Vector3d.Distance(here, there);
-				print("ArmstrongNav here: " + here);
-				print("ArmstrongNav there: " + there);
-				print("ArmstrongNav diff: " + (there - here));
-				print("ArmstrongNav time to arrival: " + dist + " at " + srfvel + " = " + (dist / srfvel));
-				//Technique 2: Calculate great circle distance from lat/long and radius
+				//Calculate great circle distance from lat/long and radius
 				double lat1 = self.latitude * Math.PI / 180, lat2 = waypoint.latitude * Math.PI / 180;
 				double longs = (self.longitude - waypoint.longitude) * Math.PI / 180;
 				double angle = Math.Acos(Math.Sin(lat1) * Math.Sin(lat2) +
 					Math.Cos(lat1) * Math.Cos(lat2) * Math.Cos(longs));
 				double circle_dist = angle * surface.Radius;
-				print("ArmstrongNav self: " + lat1 + " - " + lat2 + " - " + longs);
-				print("ArmstrongNav angle: " + (angle * 180 / Math.PI));
 				//TODO: Add the estimated time to the current mission time and report estimated arrival time
 				print(String.Format("[ArmstrongNav] {0:0.00} deg or {1:0} m at {2:0.00} m/s = {3:0.00} sec",
 					angle * 180 / Math.PI, circle_dist, srfvel, circle_dist / srfvel));
