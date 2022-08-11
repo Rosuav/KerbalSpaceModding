29ab0b317806dfdf87ede33f84dbf14e4e1ee892
Chris Angelico <rosuav@gmail.com>
2022-08-11 17:29:01 +1000

    Add biome information to the Velocimeter

--- VelocimeterModule.cs
+++ VelocimeterModule.cs
@@ -13,6 +13,12 @@ namespace Rosuav {
 		public double descent_velocity = 0.0;
 		[KSPField(isPersistant = false, guiActive = true, guiName = "Time to contact", guiFormat = "n/a", guiUnits = " sec")]
 		public double impact_time = 0.0;
+		[KSPField(isPersistant = false, guiActive = true, guiName = "Location")]
+		public string celestial_body_name = "";
+		[KSPField(isPersistant = false, guiActive = true, guiName = "Situation")]
+		public string situation = "";
+		[KSPField(isPersistant = false, guiActive = true, guiName = "Biome")]
+		public string biome = "";
 		//TODO: Also show the surface normal, or at least the magnitude of its dot product with vertical.
 
 		void FixedUpdate()
@@ -30,6 +36,29 @@ namespace Rosuav {
 			else {
 				descent_velocity = impact_time = 0.0;
 				Fields["descent_velocity"].guiFormat = Fields["impact_time"].guiFormat = "n/a";
+			}
+			//Borrowing some ideas from MechJeb here
+			celestial_body_name = self.mainBody.name;
+			if (self.landedAt != "") {
+				situation = "Landed!";
+				biome = self.landedAt;
+			} else {
+				switch (vessel.situation) {
+					case Vessel.Situations.PRELAUNCH: situation = "Pre-launch"; break;
+					case Vessel.Situations.LANDED: situation = "Landed?"; break; //Different from having LandedAt?
+					case Vessel.Situations.SPLASHED: situation = "Splashed down"; break;
+					case Vessel.Situations.FLYING:
+						if (vessel.altitude < self.mainBody.scienceValues.flyingAltitudeThreshold)
+							situation = "Flying low";
+						else situation = "Flying high";
+						break;
+					default:
+						if (vessel.altitude < self.mainBody.scienceValues.spaceAltitudeThreshold)
+							situation = "In space low";
+						else situation = "In space high";
+						break;
+				}
+				biome = self.mainBody.BiomeMap.GetAtt(self.latitude * UtilMath.Deg2Rad, self.longitude * UtilMath.Deg2Rad).name;
 			}
 		}
 	}
